6fd9e1ad6c925a72815a3ea5e00182ba443a75d6
mozart reina <mozart@mozartreina.com>
2016-01-20 13:30:53 +0100

    fix syntax err in comment controller
    Change :posts to :post in comment controller. Streamlined voting link
    params.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  belongs_to :posts
+  belongs_to :post
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   validates :body, presence: true, length: {minimum: 10}
   has_many :votes, as: :voteable
